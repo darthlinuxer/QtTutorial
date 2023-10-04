43802bfb3076f7ba19213ad5f7c9264af1b7dfa8
Camilo Chaves <camilo.chaves@capgemini.com>
2023-10-04 17:29:11 -0300

    More examples

--- 3. CPlusPlusUniqueness/CPlusPlusUniqueness/main.cpp
+++ 3. CPlusPlusUniqueness/CPlusPlusUniqueness/main.cpp
@@ -10,17 +10,22 @@ void modifyReference(int &ref) {
 }
 
 int* createIntOnHeap() {
-    int* heapVar = new int; // Allocate an integer on the heap.
+    int* heapVar = new int; // Allocate an integer on the heap. Always remember to delete the object after use
     *heapVar = 5; // Assign it a value.
+    qDebug() << "Internal Heap variable value:" << *heapVar;
+    qDebug() << "Internal Heap variable address:" << heapVar;
     return heapVar; // Return the pointer.
 }
 
 int createIntOnStack() {
-    int stackVar = 10; // Allocate an integer on the stack.
+    int stackVar = 10; // Allocate an integer on the stack. C++ manages that for us!
+    qDebug() << "Internal Stack variable value:" << stackVar;
+    qDebug() << "Internal Stack variable address:" << &stackVar;
     return stackVar; // Return the value.
 }
 
-//Local created variables cannot outlive the method where it was created... but look createOnHeap
+//Local created variables cannot outlive the method where it was created..
+//compiler will warn you try to reference it outside
 QObject& createObjectOnStack(string name) {
     QObject stackObj; // Allocate a QObject on the stack. C++ manages that for us!
     stackObj.setObjectName(name);
@@ -45,31 +50,31 @@ int main(int argc, char *argv[])
     qDebug() << "What is x address ? " << &x;
 
     // Pointers: A pointer is the holder of a memory address.
-    // A variable is created to hold the memory address of another variable
+    // the pointer variable below is created to hold the memory address of the x variable
     int *pointer = &x; // pointer points to the address of x.
-    qDebug() << "int *pointer = &x. What is pointer value ? (points to the address of x) " << pointer;
+    qDebug() << "int *pointer = &x. What is the pointer value ? (points to the address of x) " << pointer;
     qDebug() << "What pointer points to ? (points to the value of x) " << *pointer;
     qDebug() << "What is pointer memory address ? " << &pointer;
     modifyValue(pointer);
-    qDebug() << "After modifyValue x = :" << x;
+    qDebug() << "After modifyValue x = :" << x; //so yes you can modify an external variable by pointer
 
     // References
     int &reference = x; // no reference variable is created.. under the hood is always x;
-    qDebug() << "int &reference = x. What reference value ? (should be value of x)" << reference;
-    qDebug() << "What is reference address ? (should be address of x)" << &reference;
+    qDebug() << "int &reference = x. What is reference value ? (should be value of x)" << reference;
+    qDebug() << "What is reference address ? (should be address of x, because it is same as x)" << &reference;
     modifyReference(reference);
-    qDebug() << "After modifyReference:" << x;
+    qDebug() << "After modifyReference:" << x; //this is the same as byRef in C#
 
     qInfo() << "-----------------------------HEAP AND STACK ----------------------------------------";
     // Heap
     int* heapVar = createIntOnHeap();
-    qDebug() << "Heap variable value:" << *heapVar;
-    qDebug() << "Heap variable address:" << heapVar;
+    qDebug() << "External Heap variable value:" << *heapVar;
+    qDebug() << "External Heap variable address:" << heapVar;
 
     // Stack
-    int stackVar = createIntOnStack();
-    qDebug() << "Stack variable value:" << stackVar;
-    qDebug() << "Stack variable address:" << &stackVar;
+    int copiedstackVarValue = createIntOnStack();
+    qDebug() << "External Stack variable value:" << copiedstackVarValue;
+    qDebug() << "External Stack variable address:" << &copiedstackVarValue;
     delete heapVar; // Don't forget to delete heap-allocated variables!
 
     auto o1 = createObjectOnHeap("OnHeap");
