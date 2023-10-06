7b0eb5ae19c5ba95692c62c8f9d2fca105d91db5
Camilo Chaves <camilo.chaves@capgemini.com>
2023-10-06 15:54:09 -0300

    Improved Collections

--- 2. CSharpEquivalence/CSharpEquiv/Collections.cpp
+++ 2. CSharpEquivalence/CSharpEquiv/Collections.cpp
@@ -12,38 +12,51 @@
 
 void CollectionExample() {
     // QList
-    QList<int> list = {1, 2, 3};
-    list.append(4);
+    QList<int> list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    list.append(11);
     list.prepend(0);
     list.removeAt(0);
-    qInfo() << "QList:" << list;
+    std::function<bool(int)> predicate1 =  [](int value){ return value > 3; };
+    list.removeIf(predicate1);
+    qInfo() << "Filtered QList:" << list;
+
+    // Readonly Collection
+    const QList<int> constList = list;
+    qInfo() << "Readonly QList:" << constList;
 
     // QVector
-    QVector<int> vector = {1, 2, 3};
-    vector.append(4);
+    QVector<int> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector.append(11);
     qInfo() << "QVector:" << vector;
+    for(int &value : vector) { value *= 2; }
+    qInfo() << "Transformed QVector:" << vector;
 
     // QStack
     QStack<int> stack;
     stack.push(1);
+    stack.push(2);
+    stack.push(3);
     stack.pop();
     qInfo() << "QStack:" << stack;
 
     // QQueue
     QQueue<int> queue;
     queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
     queue.dequeue();
     qInfo() << "QQueue:" << queue;
 
     // QSet
-    QSet<int> set = {1, 2, 3};
-    set.insert(4);
+    QSet<int> set = {20, 1, 5, 2, 3, 4, 5, 5, 5, 5, 6, 7, 7, 8, 8, 9, 9, 10};
+    set.removeIf(predicate1);
     qInfo() << "QSet:" << set;
 
-    // QMap
-    QMap<QString, int> map;
-    map.insert("one", 1);
-    map.remove("one");
+    // QMap = Dictionary in C#
+    QMap<QString, int> map = {{"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}};
+    std::function<bool(const QString&, int&)> predicate2 =  [](const QString& key, int& value){ return value < 3; };
+    //PREDICATE ABOVE EVEN THOUGH CORRECTLY DEFINED DIDN´T WORK.. WILL SEARCH FOR A SOLUTION
+    //map.removeIf(predicate2);
     qInfo() << "QMap:" << map;
 
     // QMultiMap
@@ -53,8 +66,8 @@ void CollectionExample() {
     qInfo() << "QMultiMap:" << multiMap;
 
     // QHash
-    QHash<QString, int> hash;
-    hash.insert("one", 1);
+    QHash<QString, int> hash = {{"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}};
+    //hash.removeIf(predicate2);
     qInfo() << "QHash:" << hash;
 
     // QMultiHash
@@ -63,7 +76,4 @@ void CollectionExample() {
     multiHash.insert("one", 2);
     qInfo() << "QMultiHash:" << multiHash;
 
-    // Readonly Collection
-    const QList<int> constList = list;
-    qInfo() << "Readonly QList:" << constList;
 }
