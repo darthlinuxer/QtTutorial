d5059322591f5c4b1137cac1f7b6252f930c839e
Camilo Chaves <camilo.chaves@capgemini.com>
2023-10-13 15:51:11 -0300

    Errors and Exceptions

--- 2. CSharpEquivalence/CSharpEquiv/ErrorAndExceptions.cpp
+++ 2. CSharpEquivalence/CSharpEquiv/ErrorAndExceptions.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include "ErrorAndExceptions.h"
 
+struct MyCustomError: public std::exception
+{
+    const char* description;
+    const char* what() const noexcept
+    {
+        return "Oops! mycustomException happened! ";
+    }
+};
+
 void ErrorAndExceptionsExample()
 {
     QTextStream qin(stdin);
@@ -15,16 +24,26 @@ void ErrorAndExceptionsExample()
         QString input = qin.readLine();
         value = input.toInt();
         if (value < 0)
-          throw std::runtime_error("Value should be greater than 0");
-        if (value > 10)
-          throw QString("Value should not be greater than 10");
+          throw std::invalid_argument("Value should be greater than 0");
+        if (value == 100) {
+            MyCustomError e;
+            e.description = "I guess you don't follow rules";
+            throw e;
+        }
+        if (value > 10 and value < 20)
+          throw QString("Value should not be greater than 10 and less than 20");
+        if (value > 20)
+          throw std::out_of_range("wow this is way out of range");
+
         throw value;
       } catch (QString e) {
         qInfo() << "Caught a QString: " << e;
       } catch (int e) {
         qInfo() << "Caught an int: " << e;
-      } catch (std::exception e) {
-        qInfo() << "Caught an Exception: " << e.what();
+      } catch (MyCustomError const &e) {
+        qInfo() << e.what() << " -> " << e.description;
+      } catch (std::exception const &e) {
+        qInfo() << "Caught a Generic Exception: " << e.what();
       } catch (...) {
         qInfo() << "Caught something...";
       }
